5f9f5e687611282830c311b3ab7739531da40a73
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2021-04-10 16:10:21 -0400

    Fish paths and colours.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 if isatty
-    source "$HOME/.opam/opam-init/init.fish" > /dev/null 2> /dev/null; or true
+    source "$HOME/.opam/opam-init/init.fish"
 end
+source "$HOME/.opam/opam-init/variables.fish"
 
-source "$HOME/.opam/opam-init/variables.fish" > /dev/null 2> /dev/null; or true
+set -gp fish_user_paths "/usr/local/bin"
+set -gp fish_user_paths "$HOME/.cargo/bin"
+set -gp fish_user_paths "/usr/local/opt/ruby/bin"
+set -gp fish_user_paths (gem environment gemdir)"/bin"
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,7 @@
+function fish_prompt
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    printf '   '
+end
